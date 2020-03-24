a7d2cca0ee2cc2b0499ef7c4c89c91c65f47e0de
fanaya <fanaya@bltomato.com>
2020-03-24 11:22:55 -0700

    finishe tutorial

---
+++ ToDoList/ToDoList/TodoItem.cs
@@ -0,0 +1,13 @@
+﻿using System;
+namespace ToDoList
+{
+    public class TodoItem
+    {
+        public string Title { get; set; }
+        public bool IsDone { get; set; }
+
+        public TodoItem()
+        {
+        }
+    }
+}
